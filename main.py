5047065b1001a661b7ad1edcee6fb5ed5df56ec8
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2025-01-11 20:47:51 +0600

    Lesson 3

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from aiogram import executor
 import logging
 from config import bot, Admins, dp
-from handlers import commands, echo, quiz, webapp
+from handlers import commands, echo, quiz, webapp, FSM_registration
+from buttons import start
 
 async def on_startup(_):
     for admin in Admins:
-        await bot.send_message(chat_id=admin, text='Бок включен!')
+        await bot.send_message(chat_id=admin, text='Бок включен!', reply_markup=start)
 
 
 commands.register_handlers(dp)
 quiz.register_handlers(dp)
 webapp.register_handlers(dp)
+FSM_registration.register_handlers_fsm_reg(dp)
 
 echo.register_handlers(dp)
 
